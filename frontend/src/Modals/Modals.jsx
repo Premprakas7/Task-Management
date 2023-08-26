a93b48da6454bc089a505f529f2b2c0e60d80cf0
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-08-26 21:41:10 +0530

    updated

--- frontend/src/Modals/Modals.jsx
+++ frontend/src/Modals/Modals.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
+
+const initialState = {
+    title: "",
+    description: "Default Description",
+    task_status: "todo",
+    tags: ["Others"],
+    subTasks: [],
+  };
+  
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "title":
+        return {
+          ...state,
+          title: action.payload,
+        };
+  
+      case "description":
+        return {
+          ...state,
+          description: action.payload,
+        };
+      case "task_status":
+        return {
+          ...state,
+          task_status: action.payload,
+        };
+      case "tags":
+        return {
+          ...state,
+          tags: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+
 const Modals = () => {
   return (
     <div>
+        <Modal >
+
+        </Modal>
       
     </div>
   )
